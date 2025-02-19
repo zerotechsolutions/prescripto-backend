91a504cbbe8fa9944feba756d6722c0c62bb926c
zerotechsolution <zerotechsolutions612@gmail.com>
2025-02-19 21:42:24 +0530

    backendatabased

--- config/mongodb.js
+++ config/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
 
     mongoose.connection.on('connected', () => console.log("Database Connected"))
-    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true`)
 
 }
 
